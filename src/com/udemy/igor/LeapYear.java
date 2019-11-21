9875b2057c9e38ca748f97032620d01517ab93a5
Igorcapatina <47891605+Igorcapatina@users.noreply.github.com>
2019-11-21 11:09:55 -0800

    Update LeapYear.java

--- src/com/udemy/igor/LeapYear.java
+++ src/com/udemy/igor/LeapYear.java
@@ -2,7 +2,7 @@ package com.udemy.igor;
 
 public class LeapYear {
     public static boolean isleapYear(int year){
-        if ((year >= 0 && year <= 9999) && ((year % 400 == 0)||(year % 4 == 0 && year % 100 != 0))){
+        if ((year >= 1 && year <= 9999) && ((year % 400 == 0)||(year % 4 == 0 && year % 100 != 0))){
             return true;
         }else {return false;}
 
